dc643512d8618e90e8a377f4e168a7303b6435e8
JeremyEphron <jeremyephron@gmail.com>
2017-05-05 18:45:44 -0700

    d

--- backend/info.php
+++ backend/info.php
@@ -1,6 +1,7 @@
 <?php
-	$servername = 'localhost';
-	$username = 'root';
-	$password = 'pass';
-	$dbname = 'project_dex';
+	$servername = 'us-cdbr-iron-east-03.cleardb.net';
+	$username = 'b13958378ca589';
+	$password = '51be59bc';
+	$dbname = 'heroku_6f71a327d027584';
 ?>
+mysql://b13958378ca589:51be59bc@us-cdbr-iron-east-03.cleardb.net/heroku_6f71a327d027584?reconnect=true
