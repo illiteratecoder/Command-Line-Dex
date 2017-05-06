c908963be1c39c3a94360c482ea8c2fd7f150422
JeremyEphron <jeremyephron@gmail.com>
2017-05-05 21:36:58 -0700

    d

--- assets/js/command_line.js
+++ assets/js/command_line.js
@@ -2,6 +2,34 @@
 
 "use strict";
 
+function preloadImages(array) {
+    if (!preloadImages.list) {
+        preloadImages.list = [];
+    }
+    let list = preloadImages.list;
+    for (let i = 0; i < array.length; i++) {
+        let img = new Image();
+        img.onload = function() {
+            let index = list.indexOf(this);
+            if (index !== -1) {
+                // remove image from the array once it's loaded
+                // for memory consumption reasons
+                list.splice(index, 1);
+            }
+        }
+        list.push(img);
+        img.src = array[i];
+    }
+}
+
+let $imgArray = [];
+
+for (let i = 1; i<16; i++){
+	$imgArray.push("models/" + i + ".gif")
+}
+
+preloadImages($imgArray);
+
 let $n = null;  // number of current Pokemon
 
 // Bind functions to the input command line and run when value changes
