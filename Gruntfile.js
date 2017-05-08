854fd706c47997ba5d12d06d2c383005775f772d
JeremyEphron <jeremyephron@gmail.com>
2017-05-07 18:49:06 -0700

    d

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,12 @@
 module.exports = function(grunt) {
-  var es2015 = require('babel-preset-es2015')
-  require('load-grunt-tasks')(grunt); // npm install --save-dev load-grunt-tasks 
+
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     babel: {
         options: {
             sourceMap: false,
-            presets: ["es3932"]
+            presets: ["es2015"]
         },
         dist: {
             files: {
@@ -28,10 +27,15 @@ module.exports = function(grunt) {
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-babel')
 
   // Default task(s).
-  grunt.registerTask('default', ['babel', 'uglify']);
+  grunt.registerTask('heroku', ['babel', 'uglify']);
 };
+
+
+
+
 
 
 
