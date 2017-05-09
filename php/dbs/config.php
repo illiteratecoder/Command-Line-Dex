e653c10d0d736981257f3260433f82c244cf3722
JeremyEphron <jeremyephron@gmail.com>
2017-05-09 06:12:27 -0700

    u

---
+++ php/dbs/config.php
@@ -0,0 +1,6 @@
+<?php
+	$servername = 'jsk3f4rbvp8ayd7w.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+	$username = 'tbkzwapnl7zbvu9k';
+	$password = 'u4ndh2q3a97w0jii';
+	$dbname = 'zzqsiqznda2rxylc';
+?>
