94bf4e57d2cbca9dc1b57e7ac3ac0c91fd6c2377
Jeremy Ephron Barenholtz <illiteratecoder@users.noreply.github.com>
2017-05-09 06:06:09 -0700

    Update

--- php/dbs/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$servername = 'jsk3f4rbvp8ayd7w.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
-	$username = 'tbkzwapnl7zbvu9k';
-	$password = 'u4ndh2q3a97w0jii';
-	$dbname = 'zzqsiqznda2rxylc';
-?>
