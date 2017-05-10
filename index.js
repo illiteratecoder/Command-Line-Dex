50787d91241deb470f2465c09a94816a999c7512
JeremyEphron <jeremyephron@gmail.com>
2017-05-10 03:18:04 -0700

    d

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 /* eslint-env node */
 /* eslint no-console: "off" */
 
-var path = require('path');
-var express = require('express');
-var app = express();
-var router = express.Router();
+var path = require('path'),
+	express = require('express'),
+	app = express(),
+	router = express.Router(),
+	bodyParser = require('body-parser');
 
 // Change path for php so it works on heroku and locally.
 var phpPath = 'php';
@@ -14,51 +15,34 @@ if (process.env.PWD === '/app') {
 var phpExpress = require('php-express')({
 	binPath: phpPath
 });
-var bodyParser = require('body-parser');
-app.use(bodyParser.json());
 
+app.use(bodyParser.json());
 app.set('port', (process.env.PORT || 5000));
 
 // Set public folders.
-console.log(path.join(__dirname, '/assets'));
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 app.use('/models', express.static(path.join(__dirname, 'images/models')));
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
-// views is directory for all template files
+// Views is the directory for all template files.
 app.set('views', path.join(__dirname, 'php'));
 app.engine('php', phpExpress.engine);
 app.set('view engine', 'php');
 
-// routing all .php file to php-express
+// Route all .php files to php-express.
 app.all(/.+\.php$/, phpExpress.router);
 
-//Home Page Route
+// Set home page.
 router.get('/', function(req, res){
 	res.sendFile(path.join(__dirname, 'index.html'));
 });
+
+// Send all requests to router.
 app.use('/', router);
 
-// Route for handling PDF generation!
-// router.get('/b', function(req, res, next){
-// 	var name = req.param('name');
-// 	res.render('pokefinder', {name: name});
-// });
-// router.get('/b', function(req, res) {
-//     res.send(path.join(__dirname, '/backend/pokefinder.php'));
-// });
-
-// pdfGen.start_count();
-
-//Route for calling PDF Count!
-// app.get('/pdfcount', function(request, response){
-//   response.sendFile(path.join(__dirname, '/backend/count.txt'));
-// });
-
-
-//If no other express route captures path, return a 404 page
+// If no other express route captures a path, return a 404 page.
 app.use(function (req, res) {
-  res.status(404).send("Sorry can't find that!")
+  res.status(404).sendFile(path.join(__dirname, '404.html'))
 })
 
 app.listen(app.get('port'), function() {
